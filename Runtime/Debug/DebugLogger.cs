6a712b525a67bafcc7ef8ce31b50d933063bebdb
Sxer_lmh <308457938@qq.com>
2022-10-20 10:31:38 +0800

    添加工程

---
+++ Runtime/Debug/DebugLogger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sxer.Tool
+{
+    public static class DebugLogger
+    {
+        public static void DebugLog_(string str)
+        {
+#if UNITY_EDITOR
+            Debug.Log(str);
+#endif
+        }
+
+
+        public static void DebugLog_Warning(string str)
+        {
+#if UNITY_EDITOR
+            Debug.Log(string.Format("<color=yellow>{0}</color>", str));
+#endif
+        }
+
+        public static void DebugLog_Error(string str)
+        {
+#if UNITY_EDITOR
+            Debug.Log(string.Format("<color=red>{0}</color>", str));
+#endif
+        }
+
+    }
+}
+
